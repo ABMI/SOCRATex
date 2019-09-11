047a5a50b7bf82fcb757545c5b6fd4220bbc673d
Jimyung6642 <cory66421@gmail.com>
2019-09-11 13:54:52 +0900

    change characteristics

--- inst/app.R
+++ inst/app.R
@@ -58,20 +58,21 @@ shinyApp(
                                               , textInput('dictionary_table', 'Dictionary table', '',  placeholder = 'ex) DBName.dbo.tableName')
                                               , actionButton('process', 'Pre-Process')
                                               , width="3")
-                               , mainPanel(column(6, align='center', DT::dataTableOutput("count"))
-                                           , column(6, plotly::plotlyOutput("age"))
-                                           , column(6,plotly::plotlyOutput("pie"))
-                                           , column(6, plotly::plotlyOutput("date"))
-                               ))
+                               # , mainPanel(column(6, align='center', DT::dataTableOutput("count"))
+                               #             , column(6, plotly::plotlyOutput("age"))
+                               #             , column(6,plotly::plotlyOutput("pie"))
+                               #             , column(6, plotly::plotlyOutput("date"))
+                               # )
+                               )
                     , navbarMenu("Exploration"
-                                 # , tabPanel("Characteristics"
-                                 #            , fluidRow(column(6, align='center', DT::dataTableOutput("count"))
-                                 #                       , column(6, plotly::plotlyOutput("age"))
-                                 #            )
-                                 #            , fluidRow(column(6,plotly::plotlyOutput("pie"))
-                                 #                       , column(6, plotly::plotlyOutput("date"))
-                                 #            )
-                                 # )
+                                 , tabPanel("Characteristics"
+                                            , fluidRow(column(6, align='center', DT::dataTableOutput("count"))
+                                                       , column(6, plotly::plotlyOutput("age"))
+                                            )
+                                            , fluidRow(column(6,plotly::plotlyOutput("pie"))
+                                                       , column(6, plotly::plotlyOutput("date"))
+                                            )
+                                 )
                                  , tabPanel("LDAtuning"
                                             , fluidRow(
                                               sidebarPanel(
